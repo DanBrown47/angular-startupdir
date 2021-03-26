b6bae7d0e09e87879c5adf7d72f107f7901dbc2f
DanBrown47 <danwand47@gmail.com>
2021-03-26 20:12:17 +0530

    before payment gateway integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DirectoryComponent } from './directory/directory.component';
+import { DonationComponent } from './donation/donation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:DirectoryComponent},
+  {path:'donate', component:DonationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { DirectoryComponent } from './directory/directory.component';
+import { DonationComponent } from './donation/donation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DirectoryComponent,
+    DonationComponent
   ],
   imports: [
     BrowserModule,
